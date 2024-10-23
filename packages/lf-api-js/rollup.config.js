f2f49a0ade0e8930e15642a344f451e0760f699f
alexandria.gomez <alexandria.gomez@WATSONVILLE>
2024-10-23 15:14:01 -0400

    update to minify js

--- packages/lf-api-js/rollup.config.js
+++ packages/lf-api-js/rollup.config.js
@@ -4,6 +4,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
 
 export default [
   {
@@ -19,6 +20,7 @@ export default [
       typescript({
         declaration: false
       }),
+      terser()
     ],
   },
 ];
